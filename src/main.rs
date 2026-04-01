4d3c1a81d064bcba66bfb0b7e47c5c520275ca6e
Kaushik LS <50703773+thekaushikls@users.noreply.github.com>
2026-04-01 17:09:55 +0530

    feat(cli): renamed project to "mo"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+
+
+#[derive(Parser)]
+#[command(name="mo", about="CLI tool to log work")]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Initialize a new registry
+    Init {
+        /// Path for the registry directory
+        #[arg(long, default_value="./sample/vault")]
+        path: String,
+    },
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Init { path } => {
+            println!("Initializing at: {}", path);
+        }
+    }
 }

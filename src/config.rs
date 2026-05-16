890b94d7f690c09540760626c9038763b7a317e2
Kaushik LS <50703773+thekaushikls@users.noreply.github.com>
2026-05-16 14:42:37 +0530

    feat: add version field to mo.toml

--- src/config.rs
+++ src/config.rs
@@ -4,11 +4,17 @@ use std::{error::Error, fs, path::PathBuf};
 
 #[derive(Debug, Default, Serialize, Deserialize)]
 pub struct Vault {
+    #[serde(default = "default_version")]
+    pub version: String,
     pub path: PathBuf,
     #[serde(default)]
     pub projects: Vec<Project>,
     #[serde(default)]
     pub people: Vec<Person>,
+}
+
+fn default_version() -> String {
+    env!("CARGO_PKG_VERSION").into()
 }
 
 impl Vault {
@@ -20,6 +26,7 @@ impl Vault {
         } else {
             fs::create_dir_all(&path)?;
             let vault = Vault {
+                version: env!("CARGO_PKG_VERSION").into(),
                 path: PathBuf::from(&path),
                 ..Default::default()
             };
